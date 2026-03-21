b91ad5f9974b608de945a37dd97546385c2261a9
litvindj <one1down@icloud.com>
2026-03-21 19:19:05 +0100

    add polish seo

--- app/[lang]/layout.jsx
+++ app/[lang]/layout.jsx
@@ -150,13 +150,13 @@ export default async function LangLayout({ children, params }) {
       <head>
         <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','33819537680993763');fbq('track','PageView');` }} />
         <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=33819537680993763&ev=PageView&noscript=1" /></noscript>
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T3FLB32H');` }} />
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NGFZW2NR');` }} />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-J3CN7R2Z67" />
         <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-J3CN7R2Z67');` }} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaOrg) }} />
       </head>
       <body className="bg-dark text-white min-h-screen">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3FLB32H" height="0" width="0" style={{display:'none',visibility:'hidden'}} /></noscript>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGFZW2NR" height="0" width="0" style={{display:'none',visibility:'hidden'}} /></noscript>
         <LanguageProvider initialLang={lang}>{children}</LanguageProvider>
       </body>
     </html>
