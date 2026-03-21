dc7cad9bfe2e94a9e96fad88d56dfc79e0fd3b2b
litvindj <one1down@icloud.com>
2026-03-21 22:34:34 +0100

    add polish seo

--- app/[lang]/page.jsx
+++ app/[lang]/page.jsx
@@ -1,7 +1,7 @@
 import HomePage from '../../components/HomePage';
 
 export async function generateStaticParams() {
-  return [{ lang: 'en' }, { lang: 'ru' }];
+  return [{ lang: 'en' }, { lang: 'ru' }, { lang: 'pl' }];
 }
 
 export default async function Page({ params }) {
